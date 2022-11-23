542b0fbf23d6d8edbc416bea0795805ad5a7c654
HristiyanIvanov <116516921+HristiyanIvanov@users.noreply.github.com>
2022-11-23 14:25:43 +0200

    Update Add5or10toList.py

--- Add5or10toList.py
+++ Add5or10toList.py
@@ -1,4 +1,17 @@
 n = int(input('End of list: '))
+li = list()
+for i in range(n+1):
+    li.append(i)
+x = int(input('Enter 0 or 1: '))
+for i in range(0,len(li)):
+    if x == 0 and li[i] % 2 == 0:
+        li[i] += 5
+    if x == 1 and li[i] % 2 != 0:
+        li[i] += 10
+    print(li[i], end=' ')
+
+#Друг вариант
+n = int(input('End of list: '))
 x = int(input('Enter 0 or 1: '))
 li = list(i for i in range(0, n+1))
 if x == 0:
@@ -10,4 +23,4 @@ elif x == 1:
         li[i] += 10
     print(li)
 else:
-    print('Incorrect number')
+    print('Input 1 or 0')
