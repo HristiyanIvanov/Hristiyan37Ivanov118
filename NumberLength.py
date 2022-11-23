82c64399ce4e7d07c935c1e3350de1a128df7552
HristiyanIvanov <116516921+HristiyanIvanov@users.noreply.github.com>
2022-11-23 11:07:20 +0200

    Update NumberLength.py

--- NumberLength.py
+++ NumberLength.py
@@ -1,3 +1,7 @@
 n=int(input('Num: '))
+li = list()
+total = 0
 for i in range(1,n+1):
-    print(f'{n}'*i,end=' ')
+    li.append(int(f'{n}'*i))
+    total = total + li[-1]
+print(li, total, end=' ')
