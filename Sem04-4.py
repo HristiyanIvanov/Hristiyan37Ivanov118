afb372c6d4a5f450d57672f895445447d7c9a5f7
Hristiyan Ivanov <116516921+HristiyanIvanov@users.noreply.github.com>
2022-12-01 17:03:43 +0200

    Create Sem04-4.py

---
+++ Sem04-4.py
@@ -0,0 +1,24 @@
+def input_nums(n):
+    list = []
+    for i in range(n):
+        num = input('Enter num: ')
+        if type(num) == int or num.isnumeric():
+            list.append(num)
+    return list
+
+def sum_list(lst):
+    sum = 0
+    for i in lst:
+        if type(i) == int or type(i) == float:
+            sum += i
+        elif type(i) == str and i.isnumeric():
+            sum += float(i)
+    return sum
+
+def max_of_two(a, b):
+    if type(a)==int or type(a)==float:
+        if type(b)==int or type(b)==float:
+            return a if a >= b else b
+        return a
+    if type(b)==int or type(b)==float: return b
+    return
